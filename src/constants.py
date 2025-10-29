b34e0360053a146128473e84ee9cf9acaaa046c6
Nathan <nathnnovier@gmail.com>
2025-10-29 08:45:00 +0100

    Adding Session 4

--- src/constants.py
+++ src/constants.py
@@ -11,6 +11,8 @@ PROJECT_ROOT_PATH = Path(__file__).parents[1]
 PROJECT_ID: str | None = os.getenv("GCP_PROJECT_ID")
 REGION: str = os.getenv("GCP_REGION", "europe-west1")
 BUCKET_NAME: str | None = os.getenv("GCP_BUCKET_NAME")
+ENDPOINT_ID: str | None = os.getenv("GCP_ENDPOINT_ID")
+PROJECT_NUMBER: str | None = os.getenv("GCP_PROJECT_NUMBER")
 
 # Paths
 RAW_DATASET_URI: str = f"gs://{BUCKET_NAME}/dolly_pirate_simple_dataset.csv"
